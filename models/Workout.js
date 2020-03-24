a30b8f5f00a1acfbe83ad76d5390b0a640d3cdf7
Your Name <you@example.com>
2020-03-24 09:32:24 -0400

    day Property added to Workout Schema.

--- models/Workout.js
+++ models/Workout.js
@@ -3,10 +3,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
     name: {
         type: String,
         trim: true, 
-        required: "String Is Required"
+      //  required: "String Is Required"
     }, 
     exercises: [
         {
